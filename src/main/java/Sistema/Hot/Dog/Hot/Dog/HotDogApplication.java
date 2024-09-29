f6b31c899f3a4de16f41785bfee21498addd401c
demisetcron <albraz.v@gmail.com>
2024-09-29 00:56:42 -0300

    first commit

---
+++ src/main/java/Sistema/Hot/Dog/Hot/Dog/HotDogApplication.java
@@ -0,0 +1,13 @@
+package Sistema.Hot.Dog.Hot.Dog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotDogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotDogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Sistema/Hot/Dog/Hot/Dog/HotDogApplicationTests.java
@@ -0,0 +1,13 @@
+package Sistema.Hot.Dog.Hot.Dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotDogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
